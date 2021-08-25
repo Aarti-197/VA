22bdc2d9d0dfbc9df5faa7c4841b808b40dbdc0d
Aarti-197 <74257972+Aarti-197@users.noreply.github.com>
2021-08-25 10:52:16 +0530

    Update virtual.py

--- virtual.py
+++ virtual.py
@@ -180,11 +180,6 @@ if __name__=='__main__':
             speak(answer)
             print(answer)
 
-
-        elif "log off" in statement or "sign out" in statement:
-            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
-            subprocess.call(["shutdown", "/l"])
-
 time.sleep(3)
 
 
